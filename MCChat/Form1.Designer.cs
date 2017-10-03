a0501c9842ef7894d268e56cd311dd223784f140
Peter Hsu <pthsu@outlook.com>
2017-10-03 18:03:54 +0800

    加入MultiCast範例

---
+++ MCChat/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace MCChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnJoin = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGroupID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnJoin
+            // 
+            this.btnJoin.Location = new System.Drawing.Point(44, 53);
+            this.btnJoin.Name = "btnJoin";
+            this.btnJoin.Size = new System.Drawing.Size(100, 36);
+            this.btnJoin.TabIndex = 0;
+            this.btnJoin.Text = "加入聊天室";
+            this.btnJoin.UseVisualStyleBackColor = true;
+            this.btnJoin.Click += new System.EventHandler(this.btnJoin_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(32, 188);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(550, 196);
+            this.listBox1.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(209, 59);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(177, 22);
+            this.txtName.TabIndex = 2;
+            this.txtName.Text = "Peter";
+            // 
+            // btnSendMessage
+            // 
+            this.btnSendMessage.Location = new System.Drawing.Point(44, 109);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(94, 35);
+            this.btnSendMessage.TabIndex = 3;
+            this.btnSendMessage.Text = "送訊息";
+            this.btnSendMessage.UseVisualStyleBackColor = true;
+            this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(217, 117);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(177, 22);
+            this.txtMessage.TabIndex = 4;
+            this.txtMessage.Text = "Hello";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Group ID：";
+            // 
+            // txtGroupID
+            // 
+            this.txtGroupID.Location = new System.Drawing.Point(112, 19);
+            this.txtGroupID.Name = "txtGroupID";
+            this.txtGroupID.Size = new System.Drawing.Size(100, 22);
+            this.txtGroupID.TabIndex = 6;
+            this.txtGroupID.Text = "224.5.6.7";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(233, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Port：";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(276, 19);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 22);
+            this.txtPort.TabIndex = 8;
+            this.txtPort.Text = "4567";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(160, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "訊息：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(162, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "名字：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 501);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtGroupID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnSendMessage);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnJoin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJoin;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGroupID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MCChat/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCChat
+{
+    public partial class Form1 : Form
+    {
+        UdpClient client = null;
+        IPEndPoint multiCastEP = null;
+
+        #region 初始
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion 初始
+
+        #region 表單事件
+        private void btnJoin_Click(object sender, EventArgs e)
+        {
+            IPAddress group = IPAddress.Parse(txtGroupID.Text.Trim());
+            int port = int.Parse(txtPort.Text.Trim());
+
+            client = new UdpClient(port);
+            client.JoinMulticastGroup(group, 10);
+            multiCastEP = new IPEndPoint(group, port);
+
+            ReceiveMessage();
+
+            SendMessage("加入聊天室");
+        }
+        private void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            SendMessage(txtMessage.Text);
+        }
+        #endregion 表單事件
+
+        #region private
+        private void SendMessage(string message)
+        {
+            byte[] buff;
+            buff = Encoding.UTF8.GetBytes($"{txtName.Text} - {message}");
+            client.Send(buff, buff.Length, multiCastEP);
+        }
+
+        private async void ReceiveMessage()
+        {
+            while(true)
+            {
+                var result = await client.ReceiveAsync();
+                string message = Encoding.UTF8.GetString(result.Buffer);
+                UpdateUI_listBox1(message);
+            }
+        }
+        private void UpdateUI_listBox1(string message)
+        {
+            this.Invoke(new Action(() => {
+                listBox1.Items.Add(message);
+            }));
+        }
+        #endregion private
+
+
+    }
+}
